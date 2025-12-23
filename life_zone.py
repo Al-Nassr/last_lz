1e07adf79a80701304d725fe4026953fe43bbdb2
Al-Nassr <moisescaicedo@tutamail.com>
2025-12-23 10:00:51 +0300

    life_zone.py

--- life_zone.py
+++ life_zone.py
@@ -1,12 +1,12 @@
-import math
+import math 
+
+qwe = lambda L1, L2: math.sqrt(L1 / L2)
 
 def main():
-    Lstar = float(input("Введите болометрический показатель звезды: "))
-    Lsun = float(input("Введите болометрический показатель Солнца: "))
-
-    radius = lambda Lstar, Lsun: math.sqrt(Lstar/Lsun)
-
-    print(f"Средний радис обитаемой зоны вокруг земли: {radius(Lsun , Lstar)}")
+    L1 = float(input('показатель звезды (L1): '))
+    L2 = 3.86*10**33
+    result = qwe (L1, L2)
+    print('Средний радиус:', format(result*149597870700, '.10f'))
 
 if __name__ == "__main__":
     main()
